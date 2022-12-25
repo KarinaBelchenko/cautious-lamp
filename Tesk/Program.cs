3534859be75c2df4000fc886562fb740e7316fe2
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-25 13:39:20 +0300

    Записали решение

--- Tesk/Program.cs
+++ Tesk/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+//Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string[] array = new string[] { "Hello", "2", "world", ":-)" };
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Diffarray(string[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] =  string.Empty;
+
+    }
+}
+
+PrintArray(array);
+Diffarray(array);
+Console.Write("-> ");
+PrintArray(array);
+
+
